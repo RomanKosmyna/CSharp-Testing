2781e99b9e7987260148bc5d6516a54cc9a303b7
RomanKosmyna <roma.kosmyna@gmail.com>
2023-12-20 12:43:00 +0200

    Changed logic. Added TestSample and TestInitialization classes.

---
+++ TestingProgram/TestSample.cs
@@ -0,0 +1,26 @@
+﻿class TestSample
+{
+    public string Question {  get; set; }
+    public List<Dictionary<int, string>> Options { get; set; }
+    public Dictionary<int, string> CorrectAnswer { get; set; }
+
+    public TestSample(string question, List<Dictionary<int, string>> options, Dictionary<int, string> correctAnswer)
+    {
+        Question = question;
+        Options = options;
+        CorrectAnswer = correctAnswer;
+    }
+
+    public void RunTest()
+    {
+        Console.WriteLine(Question);
+
+        foreach(var option in Options)
+        {
+            foreach(var value in option)
+            {
+                Console.WriteLine($"{value.Key}. {value.Value}");
+            }
+        }
+    }
+}
---
+++ TestingProgram/TestsInitialization.cs
@@ -0,0 +1,26 @@
+﻿class TestsInitialization
+{
+    public List<TestSample>? ListOfTests { get; set; }
+
+    public TestsInitialization()
+    {
+        ListOfTests = [];
+        // test #1
+        string firstTestQuestion = "Which of the following methods is an entry point in the C# console program?";
+        List<Dictionary<int, string>> firstTestOptions =
+            [
+                new Dictionary<int, string> { { 1, "public static void Program()" } },
+                new Dictionary<int, string> { { 2, "public static void Main()" } },
+                new Dictionary<int, string> { { 3, "public static void main()" } },
+                new Dictionary<int, string> { { 4, "None of the above" } },
+            ];
+
+        Dictionary<int, string> firstTestCorrectAnswer = new()
+        {
+            {1, "public static void Program()"}
+        };
+        TestSample firstTest = new(firstTestQuestion, firstTestOptions, firstTestCorrectAnswer);
+        ListOfTests?.Add(firstTest);
+        // test #2
+    }
+}
